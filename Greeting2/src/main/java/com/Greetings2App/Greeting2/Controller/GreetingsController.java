b2f467b561692032a6aa6edab158b38854b4c0c7
Rohan Saxena <therohansaxena@gmail.com>
2025-03-07 11:48:21 +0530

    UC1

--- Greeting2/src/main/java/com/Greetings2App/Greeting2/Controller/GreetingsController.java
+++ Greeting2/src/main/java/com/Greetings2App/Greeting2/Controller/GreetingsController.java
@@ -1,21 +1,10 @@
 package com.Greetings2App.Greeting2.Controller;
 
 import com.Greetings2App.Greeting2.Models.Greetings;
-import com.Greetings2App.Greeting2.Service.GreetingService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @RestController
 @RequestMapping("/greetings")
 public class GreetingsController {
-    private GreetingService greetingService;
-
-    public GreetingsController (GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
-
     //UC1
     @GetMapping("/{name}")
     public Greetings getGreeting(@PathVariable String name) {
@@ -36,42 +25,5 @@ public class GreetingsController {
     public Greetings deleteGreeting(@PathVariable String name) {
         return new Greetings("Goodbye " + name + " fromDELETE");
     }
-    //UC2
-    @GetMapping("/empty")
-    public String getGreeting(){
-        return greetingService.getGreetingMessage();
-}
-
-//UC3
-@GetMapping("/firstNameLastName")
-public String getGreeting(@RequestParam(required = false)String firstName,@RequestParam(required = false)String lastName){
-    return greetingService.getGreetingMessage(firstName, lastName);
-}
-    //UC4
-    @PostMapping("/save")
-    public Greetings  savesGreeting(@RequestBody Greetings greetings) {
-        return greetingService.saveGreetingMessage(greetings.getMessage());
-}
-    //UC5
-    @GetMapping("{id}")
-    public Greetings getGreetingById(@PathVariable Long id) {
-        return greetingService.getGreetingById(id);
-}
-//UC6
-    @GetMapping("/all")
-    public List<Greetings> getAllGreetings() {
-        return greetingService.getAllGreetings();
-}
-//UC7
-    @PutMapping("/id/{id}")
-    public Greetings updateGreeting(@PathVariable Long id, @RequestBody Greetings greetings){
-        return greetingService.updateGreeting(id,greetings.getMessage());
-}
-//UC8
-    @DeleteMapping("/id/{id}")
-    public ResponseEntity<String> deleteGreeting(@PathVariable Long id){
-        greetingService.deleteGreeting(id);
-        return ResponseEntity.ok("Greeting deleted Successfully");
-}
 }
 
