a128bc7ffbf5249f6d7d06be7870ed8639078cbc
Rohan Saxena <therohansaxena@gmail.com>
2025-03-07 11:51:59 +0530

    UC4

--- Greeting2/src/main/java/com/Greetings2App/Greeting2/Models/Greetings.java
+++ Greeting2/src/main/java/com/Greetings2App/Greeting2/Models/Greetings.java
@@ -16,13 +16,13 @@ public class Greetings {
         this.message = message;
     }
 
-    public Long getId() {
-        return id;
-    }
+//    public Long getId() {
+//        return id;
+//    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
 
     public String getMessage() {
         return message;
