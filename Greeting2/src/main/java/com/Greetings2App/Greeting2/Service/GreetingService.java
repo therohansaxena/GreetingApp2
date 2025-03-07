288568d2e323221ea53c5100f180d911643344d4
Rohan Saxena <therohansaxena@gmail.com>
2025-03-07 11:50:39 +0530

    UC3

--- Greeting2/src/main/java/com/Greetings2App/Greeting2/Service/GreetingService.java
+++ Greeting2/src/main/java/com/Greetings2App/Greeting2/Service/GreetingService.java
@@ -1,21 +1,9 @@
 package com.Greetings2App.Greeting2.Service;
 
-import com.Greetings2App.Greeting2.Models.Greetings;
-import com.Greetings2App.Greeting2.Repository.GreetingRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.*;
 
 @Service
 public class GreetingService {
-    private final GreetingRepository greetingRepository;
-    public GreetingService (GreetingRepository greetingRepository){
-        this.greetingRepository = greetingRepository;
-
-
-    }
     public String getGreetingMessage() {
 
 
@@ -32,39 +20,5 @@ public class GreetingService {
         } else {
             return "Hello World";
 }
-
-
-    }
-
-    //UC4
-    public Greetings saveGreetingMessage(String message){
-//        String Greetings=new Greetings(message);
-        return greetingRepository.save(new Greetings(message));
 }
-    public Greetings getGreetingById(Long id){
-        return greetingRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Couldn't find greeting with id"+id));
 }
-    public List<Greetings> getAllGreetings(){
-        return greetingRepository.findAll();
-}
-    public Greetings updateGreeting(Long id,String newMessage){
-        Optional<Greetings> existingGreeting = greetingRepository.findById(id);
-        if(existingGreeting.isPresent()){
-            Greetings greetings=existingGreeting.get();
-            greetings.setMessage(newMessage);
-            return greetingRepository.save(greetings);
-        }
-        throw new RuntimeException(("Greeting with Id " + id +"notfound"));
-}
-    public void deleteGreeting(Long id){
-        if(!greetingRepository.existsById(id)){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Greeting not found");
-        }
-        greetingRepository.deleteById(id);
-}
-
-}
-
-
-
