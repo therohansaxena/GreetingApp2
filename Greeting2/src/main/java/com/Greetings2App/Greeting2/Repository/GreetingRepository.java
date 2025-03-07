e7513c826c6647f48561c24463bda268f8491b9a
Rohan Saxena <therohansaxena@gmail.com>
2025-03-07 11:49:44 +0530

    UC2

--- Greeting2/src/main/java/com/Greetings2App/Greeting2/Repository/GreetingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Greetings2App.Greeting2.Repository;
-
-import com.Greetings2App.Greeting2.Models.Greetings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GreetingRepository extends JpaRepository<Greetings, Long> {
-
-}
